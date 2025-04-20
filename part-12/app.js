8fa99cfc03f52670534f669e7d8fbc068f37ae19
mohit sharma <mohitsharma7258@gmail.com>
2025-04-20 17:03:11 +0530

    update

--- part-12/app.js
+++ part-12/app.js
@@ -32,6 +32,25 @@ app.get("/delete/:id", async (req, res) => {
   res.redirect("/read");
 });
 
+app.get("/edit/:id", async (req, res) => {
+  let users = await usermodel.find({ _id: req.params.id });
+  res.render("edit", { users });
+  console.log(users);
+});
+
+app.post("/update/:id", async (req, res) => {
+  let { name, email, image } = req.body;
+  let users = await usermodel.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      name,
+      email,
+      image,
+    },
+    { new: true }
+  );
+  res.redirect("/read");
+});
 app.listen(3000, () => {
   console.log(`Server runing http://localhost:3000`);
 });
