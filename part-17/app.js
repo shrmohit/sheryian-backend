212917689aad4a86fe26147d9ffac27ae1b77b90
mohit sharma <mohitsharma7258@gmail.com>
2025-04-22 14:57:44 +0530

    like

--- part-17/app.js
+++ part-17/app.js
@@ -41,6 +41,18 @@ app.post("/post", isLoggedIn, async (req, res) => {
   res.redirect("/profile");
 });
 
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  if (post.likes.indexOf(req.user.userid) == -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+  }
+  await post.save();
+  res.redirect("/profile");
+});
+
 app.post("/register", async (req, res) => {
   let { username, name, email, password, age } = req.body;
   let user = await userModel.findOne({ email });
