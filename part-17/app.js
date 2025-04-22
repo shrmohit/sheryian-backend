1c72417f363096737eac9e65e990eb2965bb88c8
mohit sharma <mohitsharma7258@gmail.com>
2025-04-23 02:17:34 +0530

    edit

--- part-17/app.js
+++ part-17/app.js
@@ -41,6 +41,20 @@ app.post("/post", isLoggedIn, async (req, res) => {
   res.redirect("/profile");
 });
 
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  res.render("edit", { post });
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content }
+  );
+  res.redirect("/profile");
+});
+
 app.post("/register", async (req, res) => {
   let { username, name, email, password, age } = req.body;
   let user = await userModel.findOne({ email });
