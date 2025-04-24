2bf2edcada67ec5fcc974997f92368c2f745f306
mohit sharma <mohitsharma7258@gmail.com>
2025-04-24 10:12:12 +0530

    multer part 20

--- part-17/app.js
+++ part-17/app.js
@@ -5,11 +5,33 @@ const postModel = require("./model/post");
 const cookiesParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(cookiesParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// crypto.randomBytes(12, (err, bytes) => {
+//   console.log(bytes.toString("hex"));
+// });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/upload"); // file saving path
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      // path.extname(file.originalname) is code se file ka extention bahar aajye ga
+      cb(null, fn);
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -17,6 +39,14 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
 });
 
 app.get("/profile", isLoggedIn, async (req, res) => {
